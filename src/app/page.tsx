b7c403ec07222313831ed46dced14989d6126a01
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-19 15:45:48 +0200

    fix: merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Presale } from "@/components/presale";
 import Hero from "@/components/hero";
-import ListTransactions from "@/components/list-transactions";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <Presale />
-      <ListTransactions />
     </main>
   );
 }
