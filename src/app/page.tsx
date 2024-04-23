07e75ac56658937bf6bdb5f7e3533a678d64e655
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-23 10:02:47 +0200

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Presale from "@/components/presale";
-import Hero from "@/components/hero";
 import Adventure from "@/components/adventure";
 import Tokenomics from "@/components/tokenomics";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
       <Presale />
       <Adventure />
       <Tokenomics />
