6acbf9f48abae8aa9c9e81d41e0fb0ce8fccd33f
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-25 10:06:06 +0200

    Testing iframe

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,3 +1,12 @@
 export default function Top() {
-  return <section className="bg-[#FFA500] h-screen"></section>;
+  return (
+    <section className="bg-[#FFA500] h-screen">
+      <iframe
+        src="/glitch/index.html"
+        title="External Page"
+        width="100%"
+        height="100%"
+      />
+    </section>
+  );
 }
