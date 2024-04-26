f94448afaab1e00f0d00bc2e553df57b72a90b76
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-26 08:19:38 +0200

    fix

--- src/components/adventure.tsx
+++ src/components/adventure.tsx
@@ -3,20 +3,19 @@ import YouTube from "react-youtube";
 import React, { useEffect, useState } from "react";
 
 export default function Adventure() {
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width <= 640) {
-        setVideoOpts({ height: "360", width: "100%" }); // Adjust for smaller screens
-      } else {
-        setVideoOpts({ height: "480", width: "640" });
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Call once on mount
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
   return <div></div>;
+}
+
+{
+  /*
+    <section
+      className="bg-[#8b8881] flex items-center w-full justify-center h-screen flex-col gap-20"
+      id="adventure"
+    >
+      <h1 className="text-white text-2xl font-bold tracking-wider ">
+        Adventure
+      </h1>
+      <YouTube videoId="kK0AHd9N7dk" opts={videoOpts} />
+    </section>
+*/
 }
