9234f75b3646bb8e00bfaa27acaa99422776db5b
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-26 08:57:08 +0200

    fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
     <header id="home">
       <nav
         className={`fixed md:h-auto top-0 left-0 py-2 w-full mx-auto flex items-center text-white min-h-[4rem]${
-          isNavOpen ? "flex flex-col gap-4 bg-black" : "h-auto"
+          isNavOpen ? "flex flex-col gap-4" : "h-auto"
         }`}
       >
         {/*
@@ -37,12 +37,12 @@ export default function Navbar() {
           onClick={toggleNav}
         >
           <FaBars
-            className={`w-10 h-10 mt-2${isNavOpen ? " h-[1000rem]" : ""}`}
+            className={`w-10 h-10 mt-2${isNavOpen ? "h-[1000rem]" : ""}`}
           />
         </div>
         <ul
           className={`flex cursor-pointer flex-col md:space-y-0 space-y-2 items-center w-fit mx-auto justify-center gap-8 xl:gap-12 min-h-10 ${
-            isNavOpen ? "h-screen flex-col flex bg-black w-full" : "hidden"
+            isNavOpen ? "flex-col flex bg-black" : "hidden"
           }`}
           onClick={toggleNav}
         >
