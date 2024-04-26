b3ee47b600581858015da6a92d4f355d065c39a8
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-26 14:14:45 +0200

    fix

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -4,15 +4,13 @@ export default function Top() {
   return (
     <section className="relative bg-[#000] h-screen">
       <Navbar />
-      <div className="">
-        <iframe
-          className="z-[100]"
-          src="https://www.firemove.de/firemove-de/sm-web/maenner/index.htm"
-          title="External Page"
-          width="100%"
-          height="100%"
-        />
-      </div>
+      <iframe
+        className="z-[100]"
+        src="https://www.firemove.de/firemove-de/sm-web/maenner/index.htm"
+        title="External Page"
+        width="100%"
+        height="100%"
+      />
     </section>
   );
 }
