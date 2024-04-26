f6cc6f12693b7fc1f721c9c6af89a78ac410b0f3
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-26 14:11:34 +0200

    fix

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -4,7 +4,7 @@ export default function Top() {
   return (
     <section className="relative bg-[#FFA500] h-screen">
       <Navbar />
-      <div className="scroll-wrapper">
+      <div className="">
         <iframe
           className="z-[100]"
           src="https://www.firemove.de/firemove-de/sm-web/maenner/index.htm"
