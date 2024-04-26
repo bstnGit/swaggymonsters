1a23808382f803f41331851dc107f666c7010712
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-26 10:05:33 +0200

    fix

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -2,15 +2,17 @@ import Navbar from "../components/navbar";
 
 export default function Top() {
   return (
-    <section className="relative bg-[#FFA500] overflow-auto h-screen">
+    <section className="relative bg-[#FFA500] h-screen">
       <Navbar />
-      <iframe
-        className="z-[100]"
-        src="https://www.firemove.de/firemove-de/sm-web/maenner/index.htm"
-        title="External Page"
-        width="100%"
-        height="100%"
-      />
+      <div className="overflow-auto h-full">
+        <iframe
+          className="z-[100]"
+          src="https://www.firemove.de/firemove-de/sm-web/maenner/index.htm"
+          title="External Page"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </section>
   );
 }
