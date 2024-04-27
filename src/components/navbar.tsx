bb667d54b948dc2c6bd12d466eecc2f567989b88
bstnGit <Jerome.Bastien.info@gmail.com>
2024-04-27 19:30:17 +0200

    fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
         </div>
         */}
         <div
-          className={`flex justify-center mx-auto cursor-pointer items-center border-solid border-4 border-black rounded-md p-2 mb-3`}
+          className={`flex justify-center mx-auto cursor-pointer items-center mb-3 border-solid border-4 border-black rounded-md bg-black p-2`}
           onClick={toggleNav}
         >
           <FaBars className="w-10 h-10" />
