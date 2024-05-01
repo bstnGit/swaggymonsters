00d6e8cd059ce6d48b55c555d9e237eb2e8616fa
lbhMichael <michael0ndukwe@gmail.com>
2024-05-01 01:11:27 +0100

    wallet button closes nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
             <a onClick={closeNav}>Socials</a>
           </Link>
 
-          <Link href="#home" className="ml-auto">
+          <Link href="#home" className="ml-auto" onClick={closeNav}>
             <WalletMultiButton className="!bg-[#ffa500]" />
           </Link>
         </ul>
