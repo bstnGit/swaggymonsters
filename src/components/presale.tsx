3d42ee827cbf2742cec6c7cb0533ecc966a62ccd
bstnGit <Jerome.Bastien.info@gmail.com>
2024-05-03 14:02:20 +0200

    fix

--- src/components/presale.tsx
+++ src/components/presale.tsx
@@ -55,7 +55,7 @@ export default function Presale() {
         </div>
       </div>
 
-      {/*
+      
       <iframe
         className="absolute z-[200]"
         src="https://www.firemove.de/swag/presale/1/index.html"
@@ -68,10 +68,10 @@ export default function Presale() {
         className="absolute z-[100]"
         src="https://www.firemove.de/swag/presale/2/index.html"
         title="Layer 1"
-        width="100%"
+        width="100%
         height="100%"
       />
-    */}
+  
       <div className="absolute top-0 left-0 w-full h-screen z-[1000]"></div>
     </section>
   );
