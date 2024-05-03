a987cace21f77a1b75dd975eb2f2ab2037c7984a
bstnGit <Jerome.Bastien.info@gmail.com>
2024-05-03 14:27:06 +0200

    fix

--- src/components/presale.tsx
+++ src/components/presale.tsx
@@ -12,66 +12,67 @@ export default function Presale() {
 
   return (
     <section
-      className="bg-[#333333] flex flex-col md:gap-8 items-center justify-center py-16"
+      className="relative bg-[#333333] items-center justify-center h-screen"
       id="presale"
     >
-      <div className="max-w-[900px] h-auto z-[2000] w-90%">
-        <div className="flex items-center justify-center flex-col mb-5">
-          <h1 className="font-bold text-3xl md:text-5xl text-white">Presale</h1>
-          <p className="text-white">102.98 SOL has already been collected!</p>
-        </div>
-        <div className="w-full md:w-fit flex flex-col gap-12 mx-auto">
-          <div className="bg-[#FFE6BA] w-full h-auto py-4 md:py-8 gap-4 px-12 items-center flex rounded-sm flex-col">
-            <h1 className="font-bold">Copy & Send SOL</h1>
-            <div className="flex md:flex-row flex-col items-center gap-4">
-              <Button className="bg-[#FFA500] hover:bg-[#fcac19] text-[#000] text-xs">
-                {walletAdress}
-              </Button>
-              <div className="w-full flex justify-end">
-                <Button
-                  className="bg-[#FFA500] hover:bg-[#fcac19] text-[#000]"
-                  onClick={() => {
-                    navigator.clipboard.writeText(walletAdress);
-                    setCopied(true);
-
-                    setTimeout(() => {
-                      setCopied(false);
-                    }, 1000);
-                  }}
-                >
-                  {!isCopied && <Copy className="h-4 w-4" />}
-                  {isCopied && <Check className="h-4 w-4" />}
+  <div className="max-w-[900px] h-auto z-[2000] w-90% absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div>
+          <div className="flex items-center justify-center flex-col mb-5">
+            <h1 className="font-bold text-3xl md:text-5xl text-white">Presale</h1>
+            <p className="text-white">102.98 SOL has already been collected!</p>
+          </div>
+          <div className="w-full md:w-fit flex flex-col gap-12 mx-auto h-auto">
+            <div className="bg-[#FFE6BA] w-full h-auto py-4 md:py-8 gap-4 px-12 items-center flex rounded-sm flex-col">
+              <h1 className="font-bold">Copy & Send SOL</h1>
+              <div className="flex md:flex-row flex-col items-center gap-4">
+                <Button className="bg-[#FFA500] hover:bg-[#fcac19] text-[#000] text-xs">
+                  {walletAdress}
                 </Button>
+                <div className="w-full flex justify-end">
+                  <Button
+                    className="bg-[#FFA500] hover:bg-[#fcac19] text-[#000]"
+                    onClick={() => {
+                      navigator.clipboard.writeText(walletAdress);
+                      setCopied(true);
+
+                      setTimeout(() => {
+                        setCopied(false);
+                      }, 1000);
+                    }}
+                  >
+                    {!isCopied && <Copy className="h-4 w-4" />}
+                    {isCopied && <Check className="h-4 w-4" />}
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="bg-[#FFE6BA] w-full h-auto pb-8 pt-4 md:pb-12 md:pt-8 gap-4 px-12 items-center flex rounded-sm flex-col">
-            <h1 className="font-bold">Connect your wallet now!</h1>
-            <Button className="py-4 md:py-6 rounded-sm px-4 bg-[#FFA500] hover:bg-[#fcac19] text-[#000]">
-              Connect Wallet
-            </Button>
+            <div className="bg-[#FFE6BA] w-full h-auto pb-8 pt-4 md:pb-12 md:pt-8 gap-4 px-12 items-center flex rounded-sm flex-col">
+              <h1 className="font-bold">Connect your wallet now!</h1>
+              <Button className="py-4 md:py-6 rounded-sm px-4 bg-[#FFA500] hover:bg-[#fcac19] text-[#000]">
+                Connect Wallet
+              </Button>
+            </div>
           </div>
         </div>
       </div>
+      <div className="relative w-full h-full">
+        <iframe
+          className="absolute top-0 left-0 z-[200] h-full"
+          src="https://www.firemove.de/swag/presale/1/index.html"
+          title="Layer 2"
+          width="100%"
+          height="100%"
+        />
 
-      <iframe
-        className="absolute z-[200]"
-        src="https://www.firemove.de/swag/presale/1/index.html"
-        title="Layer 2"
-        width="100%"
-        height="100%"
-      />
-
-      <iframe
-        className="absolute z-[100]"
-        src="https://www.firemove.de/swag/presale/2/index.html"
-        title="Layer 1"
-        width="100%"
-        height="100%"
-      />
-
-      <div className="absolute top-0 left-0 w-full h-screen z-[1000]"></div>
+        <iframe
+          className="absolute top-0 left-0 z-[100]"
+          src="https://www.firemove.de/swag/presale/2/index.html"
+          title="Layer 1"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </section>
   );
 }
--- src/components/top.tsx
+++ src/components/top.tsx
@@ -2,24 +2,25 @@ import Navbar from "./navbar";
 
 export default function Top() {
   return (
-    <section className="relative bg-[#000] h-screen">
+    <section className="flex flex-col bg-[#000] h-screen">
       <Navbar />
+      <div className="relative">
       <iframe
-        className="absolute z-[300] overflow-hidden"
+        className="absolute top-0 left-0 z-[300] overflow-hidden h-screen"
         src="https://www.firemove.de/swag/hero/1/index.html"
         title="Layer 3"
         width="100%"
         height="100%"
       />
       <iframe
-        className="absolute z-[200]"
+        className="absolute top-0 left-0 z-[200] h-screen"
         src="https://www.firemove.de/swag/hero/2/index.html"
         title="Layer 2"
         width="100%"
         height="100%"
       />
       <iframe
-        className="absolute z-[100]"
+        className="absolute top-0 left-0 z-[100] h-screen border-1"
         src="https://www.firemove.de/swag/hero/3/index.html"
         title="Layer 1"
         width="100%"
@@ -27,6 +28,7 @@ export default function Top() {
       />
 
       <div className="absolute top-0 left-0 w-full h-screen z-[1000]"></div>
+      </div>
     </section>
   );
 }
